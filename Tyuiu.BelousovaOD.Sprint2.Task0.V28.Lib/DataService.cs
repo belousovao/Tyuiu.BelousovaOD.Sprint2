ed8ecb2e91d73f808c44184a1518429fac38193c
Tyuiu.BelousovaOD@outlook.com <bbb_od@mail.ru>
2024-10-23 22:01:28 +0500

    Task0

--- Tyuiu.BelousovaOD.Sprint2.Task0.V28.Lib/DataService.cs
+++ Tyuiu.BelousovaOD.Sprint2.Task0.V28.Lib/DataService.cs
@@ -2,18 +2,19 @@
 
 namespace Tyuiu.BelousovaOD.Sprint2.Task0.V28.Lib
 {
-    public class DataService : ISprint2Task0V29
+    public class DataService : ISprint2Task0V28
     {
         public bool[] GetCompareOperations(int x, int y)
         {
-            bool[] res = new bool[6];
-            res[0] = x == y;
-            res[1] = x + 624 != y;
-            res[2] = y < x;
-            res[3] = x * 2 > y;
-            res[4] = x + 630 <= y;
-            res[5] = x - 1000 >= y;
-            return res;
+                bool[] res = new bool[6];
+                res[0] = x == y;
+                res[1] = x + 624 != y;
+                res[2] = y < x;
+                res[3] = x * 2 > y;
+                res[4] = x + 630 <= y;
+                res[5] = x - 1000 >= y;
+                return res;
+            
         }
     }
 }

1badbc05a4e6adfddc6f728b29202219674738e6
Tyuiu.BelousovaOD@outlook.com <bbb_od@mail.ru>
2024-10-24 17:42:34 +0500

    Task3

---
+++ Tyuiu.BelousovaOD.Sprint2.Task3.V8.Test/DataServiceTest.cs
@@ -0,0 +1,44 @@
+using Tyuiu.BelousovaOD.Sprint2.Task3.V8.Lib;
+namespace Tyuiu.BelousovaOD.Sprint2.Task3.V8.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = 3;
+            double res = ds.Calculate(x);
+            double wait = 0.75;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 0;
+            double res = ds.Calculate(x);
+            double wait = 0.75;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void ValidCondition3()
+        {
+            DataService ds = new DataService();
+            double x = -10;
+            double res = ds.Calculate(x);
+            double wait = 0.744;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void ValidCondition4()
+        {
+            DataService ds = new DataService();
+            double x = -20;
+            double res = ds.Calculate(x);
+            double wait = -219.75;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.BelousovaOD.Sprint2.Task3.V8/Program.cs
@@ -0,0 +1,39 @@
+﻿using Tyuiu.BelousovaOD.Sprint2.Task3.V8.Lib;
+namespace Tyuiu.BelousovaOD.Sprint2.Task3.V8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнила: Белоусова О. Д. | СМАРТб-24-1 ";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Вложенные операторы if - else                                     *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #8                                                              *");
+            Console.WriteLine("*  Выполнила: Белоусова О. Д. | СМАРТб-24-1                               *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет требуемое значение функции Y с    *");
+            Console.WriteLine("* использованием вложенных оператор if-else, где пользователь вводит      *");
+            Console.WriteLine("* значение переменной X с клавиатуры.                                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите значение переменной X: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double res = ds.Calculate(x);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Значение функции = " + res);
+
+            Console.ReadKey();
+        }
+    }
+}

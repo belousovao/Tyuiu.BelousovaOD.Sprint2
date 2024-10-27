b28a7517039dfe832f4982a8e37fbb223fee48ed
Tyuiu.BelousovaOD@outlook.com <bbb_od@mail.ru>
2024-10-27 23:09:56 +0500

    Task 7

---
+++ Tyuiu.BelousovaOD.Sprint2.Task7.V13/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.BelousovaOD.Sprint2.Task7.V13.Lib;
+namespace Tyuiu.BelousovaOD.Sprint2.Task7.V13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #2 | Выполнила: Белоусова О. Д. | СМАРТб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #13                                                             *");
+            Console.WriteLine("* Выполнила: Белоусова Ольга Дмитриевна | СМАРТб-24-1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу на C#, которая запрашивает исходные                  *");
+            Console.WriteLine("* данные (вещественные значения) и вычисляет, находится ли точка с        *");
+            Console.WriteLine("* координатами X,Y в заштрихованной области.                              *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.Write("Введите значение переменной X: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите значение переменной Y: ");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            if (ds.CheckDotInShadedArea(x, y)) Console.WriteLine("Точка находится в заштрихованной области");
+            else Console.WriteLine("Точка не находится в заштрихованной области");
+
+            Console.ReadKey();
+        }
+    }
+}
